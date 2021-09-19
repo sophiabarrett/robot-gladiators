f08cbd1867366b43ab992699a0b3337e58db47e5
Sophia Barrett <sophia@yoursummit.media>
2021-09-19 12:01:05 -0700

    initial fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,43 @@
-// set playerName from user input
+// set player robot info
 var playerName = window.prompt("What is your robot's name?", "Tony the Robot");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This is a string, good for leaving yourself a message");
-//this will do math and log 20
-console.log(10+10);
-//what is this?
-console.log("Our robot's name is " + playerName);
+console.log(playerName, playerAttack, playerHealth);
 
-// this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun!");
+// set enemy robot info
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+console.log(enemyName, enemyAttack, enemyHealth);
+
+var fight = function fight() {
+    window.alert("Welcome to Robot Gladiators!");
+
+    // update enemy health
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    // check enemy health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    // update player health
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+    // check player health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 }
 
-// fight();
+fight();
