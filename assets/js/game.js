74a6fadacf2a4ed072ef14dac72a69be42adac7e
Sophia Barrett <sophia@yoursummit.media>
2021-09-19 11:33:00 -0700

    create fight function and playerName var

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! JavaScript is running!")
+// set playerName from user input
+var playerName = window.prompt("What is your robot's name?", "Tony the Robot");
+console.log(playerName);
+
+console.log("This is a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
